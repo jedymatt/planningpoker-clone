0e78799795cca09468bb04dac48e373218769d67
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-20 11:56:45 +0800

    Create page.tsx

---
+++ src/app/[room]/page.tsx
@@ -0,0 +1,17 @@
+import {getRoomById} from "@/lib/dbQueries";
+import {notFound} from "next/navigation";
+import MainPage from "./main";
+
+export default async function Session({
+                                          params,
+                                      }: {
+    params: { room: string };
+}) {
+    const room = await getRoomById(params.room);
+
+    if (!room) {
+        notFound();
+    }
+
+    return <MainPage/>;
+}

85d7fe3a04032d2cff6e7172950f3e6f358b53ad
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-20 22:52:48 +0800

    Update card.tsx

--- src/app/[room]/card.tsx
+++ src/app/[room]/card.tsx
@@ -12,7 +12,7 @@ export function Card({ playerName, state, value }: CardProps) {
   if (!value) {
     return (
       <div className="flex flex-col items-center justify-center">
-        <div className="h-20 w-14 bg-gray-200 border rounded-md"></div>
+        <div className="h-20 w-12 bg-gray-200 border rounded-md"></div>
         <div className="mt-2 font-bold text-center">{playerName}</div>
       </div>
     );
@@ -21,7 +21,7 @@ export function Card({ playerName, state, value }: CardProps) {
   if (state === 'reveal') {
     return (
       <div className="flex flex-col items-center justify-center">
-        <div className="relative flex items-center justify-center w-14 h-20 border-2 border-blue-500 rounded-md">
+        <div className="relative flex items-center justify-center w-12 h-20 border-2 border-blue-500 rounded-md">
           <div className="absolute flex items-center justify-center w-full h-full text-2xl font-bold text-blue-500">
             {value}
           </div>
@@ -33,7 +33,7 @@ export function Card({ playerName, state, value }: CardProps) {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="h-20 w-14 bg-blue-400 border rounded-md"></div>
+      <div className="h-20 w-12 bg-blue-400 border rounded-md"></div>
       <div className="mt-2 font-bold text-center">{playerName}</div>
     </div>
   );

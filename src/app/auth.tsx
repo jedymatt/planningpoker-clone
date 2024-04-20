29eff20195b276d5f8508e74e231c1ca13fd6c13
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-20 16:52:16 +0800

    Use initial displayName if new user

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -7,8 +7,9 @@ import {
   useEffect,
   useState,
 } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/lib/firebase';
+import { User, UserSchema } from '@/lib/types';
 
 const AuthContext = createContext<User | null>(null);
 
@@ -18,7 +19,21 @@ export const AuthContextProvider = ({ children }: PropsWithChildren) => {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => setUser(user));
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(
+          UserSchema.parse({
+            ...user,
+            displayName:
+              user.displayName ||
+              localStorage.getItem('initialDisplayName') ||
+              'Unknown',
+          }),
+        );
+      } else {
+        setUser(null);
+      }
+    });
 
     return () => unsubscribe();
   }, []);

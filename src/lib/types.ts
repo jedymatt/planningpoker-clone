df9a52a0a8203587fe8bc28cef5daba0c40ef611
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-20 11:54:34 +0800

    Create types.ts

---
+++ src/lib/types.ts
@@ -0,0 +1,36 @@
+import {z} from "zod";
+
+export const UserSchema = z.object({
+    uid: z.string(),
+    displayName: z.string(),
+    isAnonymous: z.boolean(),
+});
+
+export type User = z.infer<typeof UserSchema>;
+
+export const RoomSchema = z.object({
+    name: z.string(),
+    cards: z.string().array(),
+    players: z.object({
+        userId: z.string(),
+        displayName: z.string(),
+    })
+        .array()
+        .optional()
+        .default([]),
+    votes: z
+        .record(z.string().describe("user id"), z.string().nullish().describe("card"))
+        .optional()
+        .default({}),
+    votes_history: z.record(z.string().describe("user id"), z.string().nullish().describe("card"))
+        .array()
+        .optional()
+        .default([]),
+});
+
+export const RoomCreatedSchema = RoomSchema.extend({
+    id: z.string(),
+});
+
+export type Room = z.infer<typeof RoomSchema>;
+export type RoomCreated = z.infer<typeof RoomCreatedSchema>;

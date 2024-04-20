060bf3e61afeb12316db672eefe9c5317585d9b1
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-20 11:56:25 +0800

    Create room context

---
+++ src/app/[room]/room.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {createContext, PropsWithChildren, useContext, useEffect, useState} from "react";
+import {onAuthStateChanged} from "firebase/auth";
+import {auth} from "@/lib/firebase";
+import {RoomCreated} from "@/lib/types";
+import {onRoomChanged} from "@/lib/dbQueries";
+
+const RoomContext = createContext<RoomCreated | null>(null);
+
+export const useRoomContext = () => useContext(RoomContext)
+
+export const RoomContextProvider = ({roomId, children}: PropsWithChildren<{ roomId: string }>) => {
+    const [room, setRoom] = useState<RoomCreated | null>(null);
+
+    useEffect(() => {
+        const unsubscribe = onRoomChanged(roomId, (user) => setRoom(user));
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <RoomContext.Provider value={room}>
+            {children}
+        </RoomContext.Provider>
+    );
+}

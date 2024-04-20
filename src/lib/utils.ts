0ded56b2351b5d8d1892e4e9b42a7f8c18096b18
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-20 20:22:38 +0800

    Create distributeSeat function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,37 @@
 import { twMerge } from 'tailwind-merge';
 import { type ClassValue, clsx } from 'clsx';
+import { Room } from '@/lib/types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function distributeSeat(players: Room['players']) {
+  const top: Room['players'] = [];
+  const bottom: Room['players'] = [];
+  const left: Room['players'] = [];
+  const right: Room['players'] = [];
+
+  for (let index = 0; index < players.length; index++) {
+    const player = players[index];
+    if (index === 6) {
+      left.push(player);
+      continue;
+    }
+
+    if (index === 7) {
+      right.push(player);
+      continue;
+    }
+
+    const addToBottom = top.length > bottom.length;
+
+    if (addToBottom) {
+      bottom.push(player);
+    } else {
+      top.push(player);
+    }
+  }
+
+  return { top, bottom, left, right };
 }

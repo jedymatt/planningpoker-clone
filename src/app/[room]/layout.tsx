110724cae9ad2763a87d925cb957390a27535132
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-22 18:57:50 +0800

    Implement invite prompt (#22)
    * Update main.tsx
    
    * Update layout.tsx

--- src/app/[room]/layout.tsx
+++ src/app/[room]/layout.tsx
@@ -2,11 +2,15 @@
 
 import { RoomContextProvider, useRoomContext } from '@/app/[room]/room';
 import { useAuthContext } from '@/app/auth';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { TextField } from '../_ui/TextField';
+import { usePathname } from 'next/navigation';
 
 function Navbar() {
   const room = useRoomContext();
   const user = useAuthContext();
+  const [showInvite, setShowInvite] = useState(false);
 
   return (
     <nav className="flex px-12">
@@ -31,26 +35,58 @@ function Navbar() {
             <span className="ml-2 font-bold">{user.displayName}</span>
           </div>
         )}
-        <button
-          // onClick={() => setShowingInviteModal(true)}
-          className="border-2 border-blue-500 px-3 py-2 rounded-md text-blue-500 bg-white hover:bg-blue-100 font-bold flex"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
-            />
-          </svg>
-          <span className="ml-2">Invite players</span>
-        </button>
+        <Dialog.Root open={showInvite} onOpenChange={setShowInvite}>
+          <Dialog.Trigger asChild>
+            <button className="border-2 border-blue-500 px-3 py-2 rounded-md text-blue-500 bg-white hover:bg-blue-100 font-bold flex">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z"
+                />
+              </svg>
+              <span className="ml-2">Invite players</span>
+            </button>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay className="bg-cyan-950/50 fixed inset-0" />
+            <Dialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-white shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none border px-8 py-16">
+              <Dialog.Title className="text-2xl font-bold">
+                Invite players
+              </Dialog.Title>
+              <div className="mt-12">
+                <TextField
+                  type="text"
+                  className="w-full rounded-md border"
+                  label="Game's url"
+                  value={window.location.href}
+                  onChange={() => null}
+                />
+
+                <button
+                  className="mt-4 bg-blue-500 text-white px-3 py-2 rounded-md w-full"
+                  onClick={async () => {
+                    await navigator.clipboard.writeText(window.location.href);
+                    // todo: show toast
+                    console.log('copied');
+
+                    // close dialog
+                    setShowInvite(false);
+                  }}
+                >
+                  Copy invitation link
+                </button>
+              </div>
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </div>
     </nav>
   );

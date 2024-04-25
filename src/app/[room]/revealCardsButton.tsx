57a0461d1217746037acfbd52fae3e1b659d8078
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-25 18:25:58 +0800

    Extract components into files

---
+++ src/app/[room]/revealCardsButton.tsx
@@ -0,0 +1,17 @@
+import {useRoomContext} from '@/app/[room]/room';
+import {revealCards} from '@/lib/dbQueries';
+
+export function RevealCardsButton() {
+    const room = useRoomContext()!;
+
+    return (
+        <button
+            onClick={async () => {
+                await revealCards(room.id);
+            }}
+            className="px-4 py-2 font-bold text-white rounded bg-blue-500 hover:bg-blue-400/90 transition-all"
+        >
+            Reveal cards
+        </button>
+    );
+}
---
+++ src/app/_ui/selectableCardButton.tsx
@@ -0,0 +1,31 @@
+import {cn} from '@/lib/utils';
+
+export function SelectableCardButton(props: {
+    onClick?: (value: string | null) => void;
+    selected?: boolean;
+    value: string;
+}) {
+    const onClick =
+        props.onClick &&
+        (() => props.onClick!(props.selected ? null : props.value));
+
+    return (
+        <button
+            onClick={onClick}
+            className={cn(
+                'relative flex items-center justify-center w-12 h-20 border-2 border-blue-500 rounded-md transition-all',
+                props.selected
+                    ? 'bg-blue-500 text-white -translate-y-2'
+                    : 'hover:-translate-y-1 text-blue-500 hover:bg-blue-50',
+            )}
+        >
+            <div
+                className={cn(
+                    'absolute flex items-center justify-center w-full h-full text-lg font-bold',
+                )}
+            >
+                {props.value}
+            </div>
+        </button>
+    );
+}

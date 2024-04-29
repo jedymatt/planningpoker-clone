2450e0219107e7cd091476c981c2715c58243eae
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-04-29 23:01:53 +0800

    Update Firebase configuration to use environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,14 +8,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDfZYI7o9a0D5a3OlG-n2X6gxngnVll4ks',
-  authDomain: 'planning-poker-clone-fe224.firebaseapp.com',
-  databaseURL:
-    'https://planning-poker-clone-fe224-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'planning-poker-clone-fe224',
-  storageBucket: 'planning-poker-clone-fe224.appspot.com',
-  messagingSenderId: '820267698753',
-  appId: '1:820267698753:web:108fd220ad99eb301614c0',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
